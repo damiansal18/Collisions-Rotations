4154b43f7bb0266c2c1fd6b84234a3fc21e9cdfa
Damian Salazar <DamianS1847@gmail.com>
2021-01-06 08:47:27 -0800

    collisions and rotations

---
+++ Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    private Vector3 direction = new Vector3(1, 0, 0);
+    private Vector3 angle = new Vector3(1, 1, 1);
+    private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime*speed, Space.World);
+        transform.Rotate(angle, Space.World);
+    }
+}
